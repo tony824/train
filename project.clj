cc5efb33dc87d48d613ec93d134ac2be5795bc1d
tony wang <tony.wang@kirasystems.com>
2021-04-15 14:43:22 -0400

    Bump clojure and core.async version

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.474"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.async "1.3.610"]]
   :main train.core)
